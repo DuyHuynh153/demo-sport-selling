b2322205280b544daa856fa65f4b047d834b4dd1
noodlemaster45 <nguyenquocdat12a7@gmail.com>
2022-11-19 14:34:21 +0700

    add categories

--- test.js
+++ test.js
@@ -8,44 +8,44 @@ const PRODUCT_TYPE = {
 
 
 const product = [
-  { id: 1, image: "http://static.ybox.vn/2017/5/12/1041f8a2-368f-11e7-8a7e-cac091044fd5.jpg", title: "1: Charlotte’s Web - E.B White" },
-  { id: 2, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "2: Meiko and the Fifth Treasure" },
-  { id: 3, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "3: The Outsiders" },
+  { id: 1,type:"shirt", image: "http://static.ybox.vn/2017/5/12/1041f8a2-368f-11e7-8a7e-cac091044fd5.jpg", title: "1: Áo" },
+  { id: 2, type:"pants",image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "quần" },
+  { id: 3,type:"shoes", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "giày" },
   { id: 4, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-qQB5yclfLwCxqLRpD9tY_jhDg0PVIDQl-IO03nXZUi5hDDTpDQ&s", title: "4: The House on Mango Street" },
-  { id: 5, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9cgZnVmMMj0Eb1An2Mujhbhs0ZVc5EhhY5NPhxuFyBz--M1jkWg&s", title: "5: Thirteen Reasons Why" },
-  { id: 6, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpDmlqUatqqQMgLj_gDhRX_hSRRvNXVjrgfJLEn5VNSpF40HBp&s", title: "6: Peter Pan" },
+  { id: 5,type:"shoes",type:"shirt", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9cgZnVmMMj0Eb1An2Mujhbhs0ZVc5EhhY5NPhxuFyBz--M1jkWg&s", title: "giày" },
+  { id: 6, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpDmlqUatqqQMgLj_gDhRX_hSRRvNXVjrgfJLEn5VNSpF40HBp&s", title: "giày" },
   { id: 7, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPy6hIPkdydn_GJQ7hFqBnsPSLtNNemFGazeoquHN5czXyyIu2&s", title: "7: The Old Man and the Sea" },
   { id: 8, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpDmlqUatqqQMgLj_gDhRX_hSRRvNXVjrgfJLEn5VNSpF40HBp&s", title: "8: Ernest Hemmingway" },
   { id: 9, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLvXgwoxPYiT81oxvnETd_06GEcqgLBnusANy1IbVOdtUgD3Rd&s", title: "9: The Giver - Lois Lowry" },
-  { id: 10, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "10: Number the Stars - Lois Lowry" },
-  { id: 11, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "11: A Wrinkle In Time - Madeleine L’engle" },
+  { id: 10,type:"pants", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "10: Number the Stars - Lois Lowry" },
+  { id: 11, type:"shoes",image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "11: A Wrinkle In Time - Madeleine L’engle" },
   { id: 12, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "12: Silence Once Begun" },
-  { id: 13, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "13: Sleep Donation" },
-  { id: 14, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "14: Can’t and Won’t" },
-  { id: 15, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "15: The News: A User’s Manual" },
+  { id: 13,type:"pants", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "13: Sleep Donation" },
+  { id: 14,type:"shirt", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "14: Can’t and Won’t" },
+  { id: 15,type:"shoes", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "15: The News: A User’s Manual" },
   { id: 16, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "16: Every Day Is for the Thief" },
-  { id: 17, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "17: The UnAmericans" },
+  { id: 17,type:"pants", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "17: The UnAmericans" },
   { id: 18, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "18: Kinder Than Solitude" },
-  { id: 19, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "19: What’s Important is Feeling" },
+  { id: 19,type:"shirt", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "19: What’s Important is Feeling" },
   { id: 20, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "20: Blood Will Out" },
   { id: 21, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "21: Cubed: A Secret History of the Workplace" },
   { id: 22, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "22: The Last Illusion" },
   { id: 23, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "23: Lost for Words: A Novel" },
   { id: 24, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "24: Summer House With Swimming Pool" },
   { id: 25, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "25: I’ll Be Right There" },
-  { id: 26, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "26: English Pronuciation in use" },
+  { id: 26,type:"shirt", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "26: English Pronuciation in use" },
   { id: 27, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFWJqC2-oWjd4Y41dYAwgYohlTFsWxdVmMwKI1_bCG9TMXDAJKxA&s", title: "27: Cambridge English Skills Real Listening & Speaking" },
   { id: 28, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8K9mxh-_NQEZkbeKEXl5vdKiy8nHT7zVhIL8TASmLKTbOKO0tgA&s", title: "28: Everyday Conversations English" },
 ]
-const list_element = document.getElementById("list");
-const pagination_element = document.getElementById("pagination");
+
 
 
 let perPage = 4;
 let currentPage = 1;
 let start = 0;
 let end = perPage;
-const totalPages = Math.ceil(product.length / perPage);
+let totalPages = Math.ceil(product.length / perPage);
+let productArr =[];
 const btnnext = document.querySelector(".btn-next");
 const btnprev = document.querySelector(".btn-prev");
 
@@ -55,9 +55,27 @@ function getCurrentPage(currentPage){
 }
 
 //load sản phẩm 
-function listProduct() {
+function listProduct(type) {
   html = '';
-  const content = product.map((item, index) => {
+  if (type == ''){
+    productArr = product.slice();
+  }
+  if (type =='shirt'){
+    productArr = product.filter(item =>{
+      return item.type === type;
+    })
+  }
+  if (type =='pants'){
+    productArr = product.filter(item =>{
+      return item.type === type;
+    })
+  }
+  if (type =='shoes'){
+    productArr = product.filter(item =>{
+      return item.type === type;
+    })
+  }
+  const content = productArr.map((item, index) => {
     if (index >= start && index < end) {
       html += '<div class="card">';
       html += '<a>';
@@ -70,6 +88,22 @@ function listProduct() {
   });
   document.getElementById('container__content').innerHTML = html;
 }
+// //load sản phẩm theo loại
+// function listProductType(type){
+//   html = ''
+//   const content = product.map((item,index)=>{
+//     if (index >=start && index <end &&item.type === type){
+//       html += '<div class="card">';
+//       html += '<a>';
+//       html += '<img class ="card__img" src=' + item.image + '>';
+//       html += '</a>';
+//       html += '<h3>' + item.title + '</h3>';
+//       html += '</div>';
+//       return html;
+//     }
+//   });
+//   document.getElementById('container__content').innerHTML = html;
+// }
 //khởi tạo các nút trang bằng số
 function listPage(totalPages) {
   let html = '';
@@ -143,7 +177,7 @@ function initChangePage(){
 }
 
 
-listProduct();
+listProduct('');
 listPage(totalPages);
 initChangePage();
 
